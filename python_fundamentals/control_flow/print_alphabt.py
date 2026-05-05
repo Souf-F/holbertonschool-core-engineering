eb9fd1c9ab8150bf555c9016975b59443cea15cd
Souf-F <sfecom.31000@gmail.com>
2026-05-05 15:22:18 +0200

    Remove duplicate shebang line in print_alphabt.py

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-#!/usr/bin/env python3
 import string
 
 for letter in string.ascii_lowercase:
     if letter == 'e' or letter == 'q':
         continue
     print(letter, end='')
-    
