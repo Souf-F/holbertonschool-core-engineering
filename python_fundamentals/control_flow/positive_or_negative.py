276f3f0af628c0190fae1e32ee7d4be5714b2539
Souf-F <sfecom.31000@gmail.com>
2026-05-05 13:25:19 +0200

    Add newline at the end of positive_or_negative.py for consistency

--- python_fundamentals/control_flow/positive_or_negative.py
+++ python_fundamentals/control_flow/positive_or_negative.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 number = __import__('random').randint(-10, 10)
 
 if number > 0:
@@ -7,3 +8,4 @@ elif number == 0:
     print(f"{number} is zero")
 else:
     print(f"{number} is negative")
+    
