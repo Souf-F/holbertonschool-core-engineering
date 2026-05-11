a8607d849c7c3e240ff6a129f4aeafc2a84d64cb
Souf-F <sfecom.31000@gmail.com>
2026-05-11 14:55:35 +0200

    feat: enhance Square class docstring with module description and class details

--- python_oop/classes_and_object_model/0-square.py
+++ python_oop/classes_and_object_model/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+"""
+Square module - defines a basic Square class
+"""
 
 class Square:
+    """
+    A class representing a square.
+
+    This is a basic version with no attributes or methods.
+    """
     pass
