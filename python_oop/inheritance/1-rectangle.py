7ee6eb4ee6f9313b0a2076040e8c4f583d078579
Souf-F <sfecom.31000@gmail.com>
2026-05-15 09:16:57 +0200

    feat: add Rectangle class with validation and README.md initialization

---
+++ python_oop/inheritance/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Rectangle module
+"""
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class that inherits from BaseGeometry
+
+    Attributes:
+        __width (int): Width of the rectangle (private)
+        __height (int): Height of the rectangle (private)
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialize a new Rectangle
+
+        Args:
+            width (int): Width of the rectangle
+            height (int): Height of the rectangle
+
+        Raises:
+            TypeError: If width or height is not an integer
+            ValueError: If width or height is <= 0
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
