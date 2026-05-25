50ff8c8d89b14f92ed3e3c047af2a21f7d722072
Souf-F <sfecom.31000@gmail.com>
2026-05-25 19:31:35 +0200

    refactor: update docstring in write_file function for clarity

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -3,6 +3,15 @@
 
 
 def write_file(filename="", text=""):
+    """
+    Écrit une chaîne dans un fichier UTF-8.
 
+    Args:
+        filename: Nom du fichier
+        text: Texte à écrire
+
+    Returns:
+        Nombre de caractères écrits
+    """
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
