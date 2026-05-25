1b2c96c9f95bc86d817adbb8cff5c887d2157b4f
Souf-F <sfecom.31000@gmail.com>
2026-05-25 19:30:22 +0200

    refactor: remove redundant docstring from write_file function

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -3,6 +3,6 @@
 
 
 def write_file(filename="", text=""):
-    """Écrit une chaîne dans un fichier UTF-8 et retourne le nombre de caractères écrits."""
+
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
